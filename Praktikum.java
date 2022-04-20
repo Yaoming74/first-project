4da2928182a54e1806e1a2bee8afb76b2fc73333
Yao <Mlsong404@yandex.ru>
2022-04-20 16:51:38 +0500

    second commit

--- Praktikum.java
+++ Praktikum.java
@@ -1,1 +1,12 @@
-public static void main(String[] args){}
+public static void main(String[] args){
+	public class Converter {
+    double rateUSD;
+    double rateEUR;
+    double rateJPY;
+
+    Converter(double usd, double eur, double jpy) {
+        rateUSD = usd;
+        rateEUR = eur;
+        rateJPY = jpy;
+    }
+}
