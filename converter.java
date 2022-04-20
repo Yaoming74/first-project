021fb6e5dcfc790232364c0c123fec931c0df819
Yao <Mlsong404@yandex.ru>
2022-04-20 16:52:28 +0500

    3 commit

---
+++ converter.java
@@ -0,0 +1,10 @@
+public class Converter {
+    double rateUSD;
+    double rateEUR;
+    double rateJPY;
+
+    Converter(double usd, double eur, double jpy) {
+        rateUSD = usd;
+        rateEUR = eur;
+        rateJPY = jpy;
+    }
